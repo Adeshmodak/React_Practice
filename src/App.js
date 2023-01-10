837dd79885a551b453c37859718d1fc746ffd45c
Adeshmodak <adesh2939modak@gmail.com>
2023-01-10 23:50:01 +0530

    add template

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Travomiles from "./components/Travomiles";
+
+function App() {
+  return (
+    <div >
+     <Travomiles/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Travomiles.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Travomiles.css';
+
+export default function Travomiles() {
+    return (
+        <div className="textdecorate">
+            <h1>Travomiles</h1>
+            <h4>Powered By Travel Mart</h4>
+        </div>
+    )
+}
