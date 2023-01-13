1e66e6bb3c0252f7db9a6350ae1ff36b2e73f05a
Adeshmodak <adesh2939modak@gmail.com>
2023-01-13 19:58:21 +0530

    change-2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Travomiles from "./components/Travomiles";
-
+import Form from "./components/Form";
 function App() {
   return (
     <div >
      <Travomiles/>
+     <Form/>
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+const Form=()=>
+{
+    return (
+        <div>
+            <input type = "text" placeholder = "Enter Text.." />
+            <br />
+            <br />
+            <input type = "text" placeholder = "Enter Text.." />
+            <br />
+            <br />
+            <button type = "submit">Submit</button>
+        </div>
+    );
+}
+export default Form; 
